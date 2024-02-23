2cca2c33d0d4dd4a8447e242e6e9c3f9d64aa668
Kingjifront <pradhyumsingh91@example.com>
2024-02-23 11:49:10 +0530

    data ingetion notevook

---
+++ src/ct_scan/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,35 @@
+from ct_scan.config.configuration import ConfigurationManager
+from ct_scan.components.model_trainer import Training
+from ct_scan import logger
+
+
+
+STAGE_NAME = "Training"
+
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+        
